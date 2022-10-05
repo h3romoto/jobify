61547fc992f9dd017ca759b5259c948d856ca44e
Tanaka <t.khondowe@gmail.com>
2022-10-04 19:26:21 -0500

    middleware done

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 import express from 'express'
-import notFoungMiddleware from './middlleware/not-found.js'
-
 const app = express()
 
-
 // middleware
-notFoungMiddleware
+import notFoungMiddleware from './middlleware/not-found.js'
+import errorHandlerMiddleware from './middlleware/error-handler.js'
 
 app.get('/', (req, res) => {
+  throw new Error('error my boi')
   res.send('My son, welcome home!')
 })
 
 app.use(notFoungMiddleware)
+// place error handler here at the bottom
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
