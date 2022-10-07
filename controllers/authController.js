8f8fd5a366992957eae65abaa66f3ab3bd38d38a
Tanaka <t.khondowe@gmail.com>
2022-10-06 20:11:57 -0500

    Basic user reg

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,17 @@
-const register = (req, res) => {
-  res.send('register user')
+import User from '../models/User.js'
+
+const register = async (req, res) => {
+  try {
+    const user = User.create(req.body)
+    res.status(201).json({ user })
+  } catch (error) {
+    res.status(500).json({ msg: 'Something is not right'})
+  }
 }
 const login = (req, res) => {
   res.send('user login')
 }
-const updateUser = (req, res) => {
+const updateUser = (req, res) => { 
   res.send('update user')
 }
 
