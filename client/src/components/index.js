f13db37e6b09d913e1f9a02cdea5952fa47a1978
Tanaka <t.khondowe@gmail.com>
2022-10-09 19:59:52 -0500

    Nested pages structure

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,8 @@
 import Logo from "../components/Logo";
 import FormRow from "./FormRow";
 import Alert from "./Alert";
+import SmallSideBar from "./SmallSideBar";
+import BIgSideBar from "./BIgSideBar";
+import Navbar from "./Navbar";
 
-export { Logo, FormRow, Alert}
+export { Logo, FormRow, Alert, SmallSideBar, BIgSideBar, Navbar };
