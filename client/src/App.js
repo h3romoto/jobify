808adf58a8cef3c6d604b90ac5b03b9ea3f2de86
Tanaka <t.khondowe@gmail.com>
2022-10-09 19:45:12 -0500

    Protected route done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route} from 
 'react-router-dom'
-import { Error, Landing, Register } from './pages'  
+import { Error, Landing, Register, ProtectedRoute } from './pages'  
 import { AddJob, AllJobs, Profile, Stats, SharedLayout } from './pages/dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/dashboard'>
-          <Route path='stats' element={<Stats />}></Route>
+        <Route path='/' element={
+          <ProtectedRoute>
+            <SharedLayout />
+          </ProtectedRoute>
+        }>
+          <Route index element={<Stats />}></Route>
           <Route path='all-jobs' element={<AllJobs />}></Route>
           <Route path='add-job' element={<AddJob />}></Route>
           <Route path='profile' element={<Profile />}></Route>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,10 @@
 import Landing from './Landing';
 import Register from './Register';
 import Error from './Error';
+import ProtectedRoute from './ProtectedRoute';
 
 // Components have to be imported one by one
 // importing them in an object and referencing
 // ../pages will cause some kind of stack overflow
 
-export { Error, Landing, Register } 
+export { Error, Landing, Register, ProtectedRoute } 
